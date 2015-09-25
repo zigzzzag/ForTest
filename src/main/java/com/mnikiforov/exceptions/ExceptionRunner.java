271306d25e1fd2924ff44f2d2e5fc1f025f5ae99
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-09-25 10:58:44 +0600

    add exceptions

---
+++ src/main/java/com/mnikiforov/exceptions/ExceptionRunner.java
@@ -0,0 +1,20 @@
+package com.mnikiforov.exceptions;
+
+/**
+ * Created by SBT-Nikiforov-MO on 24.09.2015.
+ */
+public class ExceptionRunner {
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (SampleException ex) {
+            System.err.println("SampleException message: " + ex.getMassage());
+            ex.printStackTrace();
+        }
+    }
+
+    private static void f() throws SampleException {
+        throw new SampleException("SampleException from f()");
+    }
+}
---
+++ src/main/java/com/mnikiforov/exceptions/SampleException.java
@@ -0,0 +1,21 @@
+package com.mnikiforov.exceptions;
+
+/**
+ * Created by SBT-Nikiforov-MO on 24.09.2015.
+ */
+public class SampleException extends Exception {
+
+    private String message;
+
+    public SampleException() {
+    }
+
+    public SampleException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    public String getMassage() {
+        return message;
+    }
+}
