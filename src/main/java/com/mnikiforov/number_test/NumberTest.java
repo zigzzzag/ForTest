58bd78a250123f225215f886b0fb5fd4af417d8f
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-10-13 13:45:29 +0600

    add number test

---
+++ src/main/java/com/mnikiforov/number_test/NumberTest.java
@@ -0,0 +1,21 @@
+package com.mnikiforov.number_test;
+
+import org.apache.commons.lang.math.NumberUtils;
+
+/**
+ * Created by sbt-nikiforov-mo on 13.10.15.
+ */
+public class NumberTest {
+
+    static String[] testArr = new String[]{"012", "01a2", "abc", "000"};
+
+    public static void main(String[] args) {
+        for (String str : testArr) {
+            System.out.println(str + " is Digits " + NumberUtils.isDigits(str));
+        }
+        System.out.println("------------------------------------------------------------------------------------");
+        for (String str : testArr) {
+            System.out.println(str + " is Number " + NumberUtils.isNumber(str));
+        }
+    }
+}
