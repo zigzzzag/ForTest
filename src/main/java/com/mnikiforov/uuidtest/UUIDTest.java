1eca12ca14b5f8143ca4934e68a606d236c24218
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-10-26 14:54:46 +0600

    add uuid test

--- src/main/java/com/mnikiforov/uuidtest/UUIDTest.java
+++ src/main/java/com/mnikiforov/uuidtest/UUIDTest.java
@@ -5,43 +5,15 @@
  */
 package com.mnikiforov.uuidtest;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.UUID;
 
 /**
- *
  * @author mnikiforov
  */
 public class UUIDTest {
 
     public static void main(String[] args) {
-
-	long p = 3557;
-	long q = 2579;
-	
-	long n = p*q;
-	
-	long f = (p-1)*(q-1);
-	
-	long e = 3;
-		
-	
-	BigDecimal d = new BigDecimal(e);
-	d= d.pow(-1);
-//	d = d.
-		
-	long c = 100;
-	long dc = (long) (Math.pow(c, e) % n);
-//	long dcc = (long) (Math.pow(dc, d) % n);
-	
-	System.out.println(d);
-	
-	System.out.println(c);
-	System.out.println(dc);
-//	System.out.println(dcc);
-
+        System.out.println(UUID.randomUUID().toString());
     }
 
 }
