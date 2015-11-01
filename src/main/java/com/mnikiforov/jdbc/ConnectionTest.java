82be831eb87cbe3898501d8696f9bfa3edbc4c28
zigzzzag <maximnikiforov1991@gmail.com>
2015-11-01 23:45:09 +0600

    jdbc: prepared stement, insert update

--- src/main/java/com/mnikiforov/jdbc/ConnectionTest.java
+++ src/main/java/com/mnikiforov/jdbc/ConnectionTest.java
@@ -1,6 +1,11 @@
 package com.mnikiforov.jdbc;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Locale;
 
 /**
@@ -22,17 +27,65 @@ public class ConnectionTest {
             System.out.println("You made it, take control your database now!");
         } else {
             System.out.println("Failed to make connection!");
+            return;
         }
 
-        System.out.println("-----------------------------------------------------------");
-        System.out.println("Table USERS:");
-        Statement statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery("select * from USERS");
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String username = rs.getString("username");
-            String email = rs.getString("email");
-            System.out.println("id=" + id + "; username=" + username + "; email=" + email);
+        try {
+            {
+                for (int i = 0; i < 100; i++) {
+                    String username = "username_" + i;
+                    String email = "email_" + i;
+                    if (insertIntoUsers(conn, username, email)) {
+                        System.out.format("insert user with username=%s, email=%s into USERS", username, email);
+                    } else {
+                        System.out.format("Failed to insert user with username=%s, email=%s into USERS", username, email);
+                    }
+                }
+            }
+
+            {
+                System.out.println();
+                System.out.println("--------------- Statement ---------------------");
+                System.out.println("Table USERS:");
+                try (Statement statement = conn.createStatement()) {
+                    ResultSet rs = statement.executeQuery("select * from USERS");
+                    while (rs.next()) {
+                        System.out.println(getFullUserInfo(rs));
+                    }
+                }
+            }
+
+            {
+                System.out.println();
+                System.out.println("--------------- PreparedStatement ---------------------");
+                System.out.println("Table USERS with username 'zigzzzag':");
+                try (PreparedStatement ps = conn.prepareStatement("select  * from USERS where username = ?")) {
+                    ps.setString(1, "zigzzzag");
+                    ResultSet rs = ps.executeQuery();
+                    while (rs.next()) {
+                        System.out.println(getFullUserInfo(rs));
+                    }
+                }
+            }
+        } finally {
+            conn.close();
         }
+    }
+
+    private static boolean insertIntoUsers(Connection conn, String username, String email) throws SQLException {
+        try (PreparedStatement ps = conn.prepareStatement("INSERT INTO Users(username, email) VALUES (?, ?)")) {
+            ps.setString(1, username);
+            ps.setString(2, email);
+            int rowInserted = ps.executeUpdate();
+            conn.commit();
+            return rowInserted == 1;
+        }
+    }
+
+    private static String getFullUserInfo(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String username = rs.getString("username");
+        String email = rs.getString("email");
+        return "id=" + id + "; username=" + username + "; email=" + email;
     }
 }
