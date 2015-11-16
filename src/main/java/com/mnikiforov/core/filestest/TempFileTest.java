558b4937a1365fe055c5483c36c8dc7bb3703d14
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-11-16 13:31:26 +0600

    add temp file test

---
+++ src/main/java/com/mnikiforov/core/filestest/TempFileTest.java
@@ -0,0 +1,23 @@
+package com.mnikiforov.core.filestest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by sbt-nikiforov-mo on 16.11.15.
+ */
+public class TempFileTest {
+
+    public static void main(String[] args) throws IOException {
+        File file = File.createTempFile("test", ".txt");
+
+        PrintWriter writer = new PrintWriter(file);
+        try {
+            writer.write("Hello");
+            writer.write("world");
+        } finally {
+            writer.close();
+        }
+    }
+}
