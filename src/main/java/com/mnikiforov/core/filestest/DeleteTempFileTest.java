6f31361b34255d4a3d17158705d7b74b4d459161
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-11-18 15:18:03 +0600

    delete temp file test

---
+++ src/main/java/com/mnikiforov/core/filestest/DeleteTempFileTest.java
@@ -0,0 +1,36 @@
+package com.mnikiforov.core.filestest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by sbt-nikiforov-mo on 18.11.15.
+ */
+public class DeleteTempFileTest {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        File tempFile = File.createTempFile("test", ".txt");
+
+        if (tempFile.delete()) {
+            System.out.println("delete successfully");
+        } else {
+            System.out.println("delete fails");
+        }
+
+        tempFile.deleteOnExit();
+
+
+        PrintWriter writer = new PrintWriter(tempFile);
+        try {
+            writer.write("Hello");
+            writer.write("world");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            writer.close();
+        }
+
+        Thread.sleep(10_000);
+    }
+}
