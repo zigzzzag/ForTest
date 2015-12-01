77f30613c9339d94f13e90700e5fa42f56c2537b
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-12-01 17:54:33 +0600

    add query object test 2 hibernate

--- src/main/java/com/mnikiforov/hibernate/java_brains/h_query_object/QueryObjectTest.java
+++ src/main/java/com/mnikiforov/hibernate/java_brains/h_query_object/QueryObjectTest.java
@@ -29,5 +29,24 @@ public class QueryObjectTest {
                 session.getTransaction().commit();
             }
         }
+
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println();
+
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory()) {
+            try (Session session = sessionFactory.openSession()) {
+                session.beginTransaction();
+
+                Query query = session.createQuery("from Product where id > :id and serialNumber = :serialNumber");
+                query.setInteger("id", 5);
+                query.setString("serialNumber", "serial_number_6");
+                List<Product> products = query.list();
+
+                System.out.println(StringUtils.join(products, System.lineSeparator()));
+
+                session.getTransaction().commit();
+            }
+        }
     }
 }
