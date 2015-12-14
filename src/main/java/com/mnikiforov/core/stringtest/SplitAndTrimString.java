189540c04de9060c981d6065fb10b84f09febaf7
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2015-12-14 15:13:30 +0600

    add split and trim string

---
+++ src/main/java/com/mnikiforov/core/stringtest/SplitAndTrimString.java
@@ -0,0 +1,33 @@
+package com.mnikiforov.core.stringtest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by sbt-nikiforov-mo on 14.12.15.
+ */
+public class SplitAndTrimString {
+
+    static String TEST_STRING = "aaa , ,, bbb ,";
+
+    public static void main(String[] args) {
+        Set<String> strs = splitAndTrim(TEST_STRING, ",");
+        for (String str : strs) {
+            System.out.println(str);
+        }
+    }
+
+    private static Set<String> splitAndTrim(String value, String delimiter) {
+        String[] parts = value.split(delimiter);
+        Set<String> result = new HashSet<String>();
+
+        for (String part : parts) {
+            part = part.trim();
+
+            if (!part.isEmpty()) {
+                result.add(part);
+            }
+        }
+        return result;
+    }
+}
