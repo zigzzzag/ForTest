0f376b7e0e74dac398aef92bc74be88d39910cef
Nikiforov Maxim <MONikiforov.SBT@sberbank.ru>
2016-03-01 12:44:11 +0600

    modified list test
    add operation remove

--- src/main/java/com/mnikiforov/core/containers/collection/CollectionUtils.java
+++ src/main/java/com/mnikiforov/core/containers/collection/CollectionUtils.java
@@ -18,7 +18,19 @@ public class CollectionUtils {
         return list;
     }
 
+    public static void removeFromMiddle(List<Integer> list, int count) {
+        for (int i = 0; i < count; i++) {
+            list.remove(list.size() / 2);
+        }
+    }
+
     public static void insertInMiddle(List<Integer> list, int count) {
+        for (int i = 0; i < count; i++) {
+            list.add(list.size() / 2, -i);
+        }
+    }
+
+    public static void insertInMiddleWithIterator(List<Integer> list, int count) {
         ListIterator<Integer> it = list.listIterator();
         for (int i = 0; i < list.size() / 2; i++) {
             it.next();
@@ -26,6 +38,18 @@ public class CollectionUtils {
 
         for (int i = 0; i < count; i++) {
             it.add(-i);
+        }
+    }
+
+    public static void removeFromMiddleWithIterator(List<Integer> list, int count) {
+        ListIterator<Integer> it = list.listIterator();
+        for (int i = 0; i < (list.size() - count) / 2; i++) {
+            it.next();
+        }
+
+        for (int i = 0; i < count; i++) {
+            it.next();
+            it.remove();
         }
     }
 }
--- src/main/java/com/mnikiforov/core/containers/collection/ListTest.java
+++ src/main/java/com/mnikiforov/core/containers/collection/ListTest.java
@@ -18,15 +18,35 @@ public class ListTest {
     }
 
     private static void listTest(List<Integer> list) throws IllegalAccessException, InstantiationException {
+        String formatStr = "%-12s: %-40s count: %-10d size: %-10d time: %6dms%n";
+
         long startTime = System.currentTimeMillis();
         CollectionUtils.fill(list, DEFAULT_SIZE);
-        System.err.println("fill " + list.getClass().getSimpleName() + " size: " + list.size()
-                + ", initTime: " + (System.currentTimeMillis() - startTime) + "ms");
+        System.err.printf("fill %s size: %-10d initTime: %dms%n",
+                list.getClass().getSimpleName(), list.size(), System.currentTimeMillis() - startTime);
 
         startTime = System.currentTimeMillis();
         CollectionUtils.insertInMiddle(list, INSERT_COUNT);
-        System.err.println(list.getClass().getSimpleName() + " insert in middle; count: " + INSERT_COUNT
-                + ", size: " + list.size()
-                + ", time:" + (System.currentTimeMillis() - startTime) + "ms");
+        System.err.printf(formatStr,
+                list.getClass().getSimpleName(), "insert in middle", INSERT_COUNT, list.size(),
+                System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        CollectionUtils.removeFromMiddle(list, INSERT_COUNT);
+        System.err.printf(formatStr,
+                list.getClass().getSimpleName(), "remove from middle", INSERT_COUNT, list.size(),
+                System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        CollectionUtils.insertInMiddleWithIterator(list, INSERT_COUNT);
+        System.err.printf(formatStr,
+                list.getClass().getSimpleName(), "insert in middle with iterator", INSERT_COUNT, list.size(),
+                System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        CollectionUtils.removeFromMiddleWithIterator(list, INSERT_COUNT);
+        System.err.printf(formatStr,
+                list.getClass().getSimpleName(), "remove from middle with iterator", INSERT_COUNT, list.size(),
+                System.currentTimeMillis() - startTime);
     }
 }
