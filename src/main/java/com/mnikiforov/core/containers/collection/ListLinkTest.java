27781c89c7173cc245f7d5486835744b2d95fb8d
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-05-11 16:15:38 +0600

    add ListLinkTest 2

--- src/main/java/com/mnikiforov/core/containers/collection/ListLinkTest.java
+++ src/main/java/com/mnikiforov/core/containers/collection/ListLinkTest.java
@@ -11,18 +11,18 @@ public class ListLinkTest {
 
     private Integer int1;
     private Integer int2;
-    private Integer int3;
-    private Integer int4;
-    private List<Integer> testList = Arrays.asList(int1, int2, int3, int4);
+    private List<Integer> testList = Arrays.asList(int1, int2);
     private List<Integer> testList2 = new ArrayList<>();
 
     public ListLinkTest() {
+        Integer int3 = null;
         testList2.add(int1);
+        testList2.add(int3);
 
         int1 = new Integer(1);
         int2 = new Integer(2);
         int3 = new Integer(3);
-        int4 = new Integer(4);
+//        int4 = new Integer(4);
 
         testList2.add(int2);
 
