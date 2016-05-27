06540f1b08df64f434a497b77bb689f319fb6f96
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-05-27 14:35:47 +0600

    string null test

--- src/main/java/com/mnikiforov/core/stringtest/StringEqualsTest.java
+++ src/main/java/com/mnikiforov/core/stringtest/StringEqualsTest.java
@@ -8,7 +8,9 @@ public class StringEqualsTest {
     public static void main(String[] args) {
         String a = "aaa";
         String b = "aaa";
+        String nullString = null;
         System.out.println("a==b - " + (a==b));
         System.out.println("a.equals(b) - " + a.equals(b));
+        System.out.println("a.equals(null) - " + a.equals(nullString));
     }
 }
