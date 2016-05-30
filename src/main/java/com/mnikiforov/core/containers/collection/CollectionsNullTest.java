91a30a22e891cc57974ff9a5e6eae092344c930a
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-05-30 12:40:44 +0600

    add collection null test

---
+++ src/main/java/com/mnikiforov/core/containers/collection/CollectionsNullTest.java
@@ -0,0 +1,16 @@
+package com.mnikiforov.core.containers.collection;
+
+import java.util.List;
+
+/**
+ * Created by sbt-nikiforov-mo on 30.05.16.
+ */
+public class CollectionsNullTest {
+
+    public static void main(String[] args) {
+        List<String> nullList = null;
+        for (String str : nullList) {
+            System.out.println(str);
+        }
+    }
+}
