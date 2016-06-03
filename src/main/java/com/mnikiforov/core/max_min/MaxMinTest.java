cf6ee7621cf9029a34e28c386fb1407d0db0e1c6
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-06-03 11:03:53 +0600

    add max min test

---
+++ src/main/java/com/mnikiforov/core/max_min/MaxMinTest.java
@@ -0,0 +1,23 @@
+package com.mnikiforov.core.max_min;
+
+import org.apache.commons.lang.ObjectUtils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by sbt-nikiforov-mo on 03.06.16.
+ */
+public class MaxMinTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 1970);
+        Date date1 = cal.getTime();
+        Thread.sleep(10);
+        Date date2 = new Date();
+
+        System.out.println("max = " + ObjectUtils.max(date1, date2));
+        System.out.println("min = " + ObjectUtils.min(date1, date2));
+    }
+}
