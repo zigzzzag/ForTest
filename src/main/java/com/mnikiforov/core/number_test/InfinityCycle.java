1061433c20a9dbefd5146a8f066f920b9f3f6c59
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-06-07 12:03:49 +0600

    add infinity cycle test

---
+++ src/main/java/com/mnikiforov/core/number_test/InfinityCycle.java
@@ -0,0 +1,21 @@
+package com.mnikiforov.core.number_test;
+
+/**
+ * Created by sbt-nikiforov-mo on 07.06.16.
+ */
+public class InfinityCycle {
+
+    public static void main(String[] args) {
+        double i = 0d;
+        while (i != 10) {
+            System.out.println(i);
+            i += 0.1;
+
+            //break
+            if (i > 50) {
+                break;
+            }
+        }
+        System.out.println("finished!");
+    }
+}
