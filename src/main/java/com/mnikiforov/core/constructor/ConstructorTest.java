4ba45dbf01e3db9c1f4c7429d98e60a1b14e17e7
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-06-14 11:43:53 +0600

    add constructor init test

---
+++ src/main/java/com/mnikiforov/core/constructor/ConstructorTest.java
@@ -0,0 +1,25 @@
+package com.mnikiforov.core.constructor;
+
+/**
+ * Created by sbt-nikiforov-mo on 14.06.16.
+ */
+public class ConstructorTest {
+
+    public static ConstructorTest instance = new ConstructorTest();
+    private static final int DELTA = 5;
+    private static int BASE = 7;
+    private int x;
+
+    public ConstructorTest() {
+        x = BASE + DELTA;
+    }
+
+    public static int getBASE() {
+        return BASE;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ConstructorTest.instance.x);
+        System.out.println(new ConstructorTest().x);
+    }
+}
