a3704d4fcaf5ddbc49e15f9fd62f71f606db2d13
zigzzzag <maximnikiforov1991@gmail.com>
2016-06-15 00:20:33 +0600

    add ObjectAnalyzer - step 2

--- src/main/java/com/mnikiforov/core/reflection/ObjectAnalyzer.java
+++ src/main/java/com/mnikiforov/core/reflection/ObjectAnalyzer.java
@@ -1,6 +1,7 @@
 package com.mnikiforov.core.reflection;
 
 import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -10,6 +11,8 @@ import java.util.ArrayList;
  */
 public class ObjectAnalyzer {
 
+    private static ArrayList<Object> visited = new ArrayList<>();
+
     /**
      * Преобразует объект в строковое представление всех перечисляемых полей
      *
@@ -17,34 +20,49 @@ public class ObjectAnalyzer {
      * @return Возвращает строку с именем класса и всеми полями объекта, а также их значениями
      */
     public static String toString(Object obj) {
-        if (obj == null) {
-            return "null";
+        if (obj == null) return "null";
+
+        if (visited.contains(obj)) return "...";
+        visited.add(obj);
+
+        Class clazz = obj.getClass();
+        if (clazz == String.class) return (String) obj;
+
+        if (clazz.isArray()) {
+            String result = clazz.getComponentType() + "[]{";
+            for (int i = 0; i < Array.getLength(obj); i++) {
+                if (i > 0) result += ",";
+                Object val = Array.get(obj, i);
+                if (clazz.getComponentType().isPrimitive()) {
+                    result += val;
+                } else {
+                    result += toString(val);
+                }
+            }
+            return result + "}";
         }
 
-        //TODO remake
-        return null;
-    }
-
-    @Deprecated
-    public static String objectToString(Object obj) {
-        Class clazz = obj.getClass();
-
-        //TODO remake on StringBuilder
         String result = clazz.getName();
         do {
             result += "[";
             Field[] fields = clazz.getDeclaredFields();
             AccessibleObject.setAccessible(fields, true);
 
-            for (Field f : fields) {
+            for (int i = 0; i < fields.length; i++) {
+                Field f = fields[i];
                 if (!Modifier.isStatic(f.getModifiers())) {
-                    if (!result.endsWith("[")) {
+                    if (i > 0) {
                         result += ",";
                     }
                     result += f.getName() + "=";
                     try {
+                        Class type = f.getType();
                         Object val = f.get(obj);
-                        result += toString(val);
+                        if (type.isPrimitive()) {
+                            result += val;
+                        } else {
+                            result += toString(val);
+                        }
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
                     }
@@ -58,11 +76,16 @@ public class ObjectAnalyzer {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> squares = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            squares.add(i * i);
-        }
+//        int[] testArr = new int[5];
+//        for (int i = 0; i < 5; i++) {
+//            testArr[i] = i * i;
+//        }
+//        System.out.println(ObjectAnalyzer.toString(testArr));
 
-        System.out.println(ObjectAnalyzer.objectToString(squares));
+        ArrayList<Integer> squares = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) squares.add(i * i);
+
+        //TODO find bug
+        System.out.println(ObjectAnalyzer.toString(squares));
     }
 }
