750eeb3f4f32f0bf33d02295b54421dd74177d48
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-06-27 11:16:52 +0600

    add convert util to sql date

---
+++ src/main/java/com/mnikiforov/core/datetest/datesqltest/UtilDateToSqlDate.java
@@ -0,0 +1,17 @@
+package com.mnikiforov.core.datetest.datesqltest;
+
+
+/**
+ * Created by sbt-nikiforov-mo on 27.06.16.
+ */
+public class UtilDateToSqlDate {
+
+    public static void main(String[] args) {
+        java.util.Date today = new java.util.Date();
+        System.out.println("util: " + today + ", sql: " + convertToSqlDate(today));
+    }
+
+    public static java.sql.Date convertToSqlDate(java.util.Date utilDate) {
+        return new java.sql.Date(utilDate.getTime());
+    }
+}
