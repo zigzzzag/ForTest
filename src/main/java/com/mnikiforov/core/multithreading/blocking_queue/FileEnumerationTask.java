9c01ce5ddcaceef6d28e0af97f9b79e801c554e7
zigzzzag <maximnikiforov1991@gmail.com>
2016-08-13 00:14:55 +0600

    add blocking queue test - finished!

--- src/main/java/com/mnikiforov/core/multithreading/blocking_queue/FileEnumerationTask.java
+++ src/main/java/com/mnikiforov/core/multithreading/blocking_queue/FileEnumerationTask.java
@@ -4,9 +4,50 @@ import java.io.File;
 import java.util.concurrent.BlockingQueue;
 
 /**
+ * Перечисляет все файлы в каталоге и подкаталогах
+ * <p>
  * Created by sbt-nikiforov-mo on 12.08.16.
  */
-public class FileEnumerationTask {
-    public FileEnumerationTask(BlockingQueue<File> queue, File file) {
+public class FileEnumerationTask implements Runnable {
+
+    public static File DUMMY = new File("");
+    private BlockingQueue<File> queue;
+    private File startingDirectory;
+
+    /**
+     * Конструирует объект типа {@link FileEnumerationTask}
+     *
+     * @param queue             Блокирующая очередь, в которую вводятся перечисляемые файлы
+     * @param startingDirectory Каталог, с которого начинается перечисление файлов
+     */
+    public FileEnumerationTask(BlockingQueue<File> queue, File startingDirectory) {
+        this.queue = queue;
+        this.startingDirectory = startingDirectory;
+    }
+
+    @Override
+    public void run() {
+        try {
+            enumerate(startingDirectory);
+            queue.put(DUMMY);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Рекурсивно перечисляет все файлы в данном каталоге и его подкаталогах
+     *
+     * @param directory Исходный каталог
+     */
+    private void enumerate(File directory) throws InterruptedException {
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                enumerate(file);
+            } else {
+                queue.put(file);
+            }
+        }
     }
 }
--- src/main/java/com/mnikiforov/core/multithreading/blocking_queue/SearchTask.java
+++ src/main/java/com/mnikiforov/core/multithreading/blocking_queue/SearchTask.java
@@ -1,13 +1,64 @@
 package com.mnikiforov.core.multithreading.blocking_queue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 
 /**
+ * Осуществляет поиск заданного ключевого слова
+ * <p>
  * Created by sbt-nikiforov-mo on 12.08.16.
  */
 public class SearchTask implements Runnable {
 
+    BlockingQueue<File> queue;
+    String keyword;
+
+    /**
+     * Конструирует объект {@link SearchTask}
+     *
+     * @param queue   Очередь, из которой извлекаются файлы
+     * @param keyword Ключевое слово
+     */
     public SearchTask(BlockingQueue<File> queue, String keyword) {
+        this.queue = queue;
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void run() {
+        try {
+            boolean done = false;
+            while (!done) {
+                File file = queue.take();
+                if (file == FileEnumerationTask.DUMMY) {
+                    queue.put(file);
+                    done = true;
+                } else {
+                    search(file);
+                }
+            }
+        } catch (InterruptedException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Осуществляет поиск в файле заданного ключевого слова и выводит все строки, содержащие это слово
+     *
+     * @param file Файл для поиска ключевого слова
+     */
+    private void search(File file) throws FileNotFoundException {
+        try (Scanner in = new Scanner(file)) {
+            int lineNumber = 0;
+            while (in.hasNextLine()) {
+                lineNumber++;
+                String line = in.nextLine();
+                if (line.contains(keyword)) {
+                    System.out.printf("%s:%d:%s%n", file.getPath(), lineNumber, line);
+                }
+            }
+        }
     }
 }
