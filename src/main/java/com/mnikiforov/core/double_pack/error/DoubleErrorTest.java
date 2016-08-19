0a1ff2af841f5d06270d5ded7689210dcb4b946b
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-08-19 16:18:41 +0700

    add double error test

---
+++ src/main/java/com/mnikiforov/core/double_pack/error/DoubleErrorTest.java
@@ -0,0 +1,12 @@
+package com.mnikiforov.core.double_pack.error;
+
+/**
+ * Created by sbt-nikiforov-mo on 19.08.16.
+ */
+public class DoubleErrorTest {
+
+    public static void main(String[] args) {
+        System.out.println(2.0 - 1.4);
+        System.out.println(2.0 - 1.1 == 0.9);
+    }
+}
