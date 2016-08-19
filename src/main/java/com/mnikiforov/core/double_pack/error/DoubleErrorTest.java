60c8094aa19842575792c2b2d434f3280f2c1304
sbt-nikiforov-mo <MONikiforov.SBT@sberbank.ru>
2016-08-19 16:29:36 +0700

    add double error test

--- src/main/java/com/mnikiforov/core/double_pack/error/DoubleErrorTest.java
+++ src/main/java/com/mnikiforov/core/double_pack/error/DoubleErrorTest.java
@@ -6,7 +6,7 @@ package com.mnikiforov.core.double_pack.error;
 public class DoubleErrorTest {
 
     public static void main(String[] args) {
-        System.out.println(2.0 - 1.4);
+        System.out.println(2.0 - 1.1);
         System.out.println(2.0 - 1.1 == 0.9);
     }
 }
