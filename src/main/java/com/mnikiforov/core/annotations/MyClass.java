b1d41013650ccff911519e36d9c8caec3e66c502
Максим Никифоров <MONikiforov.SBT@sberbank.ru>
2016-10-06 18:54:25 +0700

    add simple annotation

---
+++ src/main/java/com/mnikiforov/core/annotations/MyClass.java
@@ -0,0 +1,10 @@
+package com.mnikiforov.core.annotations;
+
+/**
+ * Created by sbt-nikiforov-mo on 10/6/16.
+ */
+public class MyClass {
+
+    @SimleAnnotation("some text...")
+    private String myString;
+}
---
+++ src/main/java/com/mnikiforov/core/annotations/SimleAnnotation.java
@@ -0,0 +1,9 @@
+package com.mnikiforov.core.annotations;
+
+/**
+ * Created by sbt-nikiforov-mo on 10/6/16.
+ */
+public @interface SimleAnnotation {
+
+    String value();
+}
