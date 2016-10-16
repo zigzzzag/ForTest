95ef813ec8a3c6495b15cd4eaf0a944bf7360561
zigzzzag <maximnikiforov1991@gmail.com>
2016-10-16 16:52:24 +0600

    add testing sber primirives

---
+++ src/main/java/com/mnikiforov/testing_sber/t2016/c4_primitives/ByteTest.java
@@ -0,0 +1,12 @@
+package com.mnikiforov.testing_sber.t2016.c4_primitives;
+
+/**
+ * Created by Zigzag on 16.10.2016.
+ */
+public class ByteTest {
+
+    public static void main(String[] args) {
+        byte a = 40, b = 50;
+//        byte sum = (byte) a + b;//Error:(10, 29) java: possible loss of precision; required: byte, found:    int
+    }
+}
---
+++ src/main/java/com/mnikiforov/testing_sber/t2016/c4_primitives/EqualsTest.java
@@ -0,0 +1,17 @@
+package com.mnikiforov.testing_sber.t2016.c4_primitives;
+
+/**
+ * Created by Zigzag on 16.10.2016.
+ */
+public class EqualsTest {
+
+    public static void main(String[] args) {
+        int x = 0;
+        String s = null;
+//        if (x == s) { //compile error   java: incomparable types: int and java.lang.String
+//            System.out.println("Succes");
+//        } else {
+//            System.out.println("Failure");
+//        }
+    }
+}
