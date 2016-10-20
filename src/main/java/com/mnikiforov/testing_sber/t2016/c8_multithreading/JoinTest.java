24ecf1bb12929823a34cc5f18d4320b7dd505aa1
Максим Никифоров <MONikiforov.SBT@sberbank.ru>
2016-10-20 15:40:44 +0700

    add join test

---
+++ src/main/java/com/mnikiforov/testing_sber/t2016/c8_multithreading/JoinTest.java
@@ -0,0 +1,42 @@
+package com.mnikiforov.testing_sber.t2016.c8_multithreading;
+
+/**
+ * Created by sbt-nikiforov-mo on 10/20/16.
+ */
+public class JoinTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("2. start thread " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("3. finish thread " + Thread.currentThread().getName());
+            }
+        };
+
+        // with t.join
+        {
+            System.out.println("1. start JoinTest");
+            Thread t = new Thread(r);
+            t.start();
+            t.join();
+            System.out.println("4. finish JoinTest");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        // without t.join
+        {
+            System.out.println("1. start JoinTest");
+            Thread t = new Thread(r);
+            t.start();
+            System.out.println("4. finish JoinTest");
+        }
+    }
+}
