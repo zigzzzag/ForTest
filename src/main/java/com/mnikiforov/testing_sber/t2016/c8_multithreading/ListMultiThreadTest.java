ee930e8d01d0d3e30fc84275adce4b2fa7df6a90
Максим Никифоров <MONikiforov.SBT@sberbank.ru>
2016-10-24 12:18:46 +0700

    add list multi thread test

---
+++ src/main/java/com/mnikiforov/testing_sber/t2016/c8_multithreading/ListMultiThreadTest.java
@@ -0,0 +1,31 @@
+package com.mnikiforov.testing_sber.t2016.c8_multithreading;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by sbt-nikiforov-mo on 10/24/16.
+ */
+public class ListMultiThreadTest {
+
+    //    static List<String> list = Collections.synchronizedList(new ArrayList<String>());
+    static List<String> list = new ArrayList<String>();
+
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 100_000; i++) {
+                synchronized (list) {
+                    list.add("item" + i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new MyThread().start();
+        }
+    }
+}
