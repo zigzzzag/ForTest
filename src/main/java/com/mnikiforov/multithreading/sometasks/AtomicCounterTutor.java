2fe695d9e7b68753ab22d88e61dcdd8357fe5160
zigzzzag <maximnikiforov1991@gmail.com>
2017-01-22 19:06:30 +0600

    atomic counter tutor

---
+++ src/main/java/com/mnikiforov/multithreading/sometasks/AtomicCounterTutor.java
@@ -0,0 +1,54 @@
+package com.mnikiforov.multithreading.sometasks;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by zigzzzag on 22.01.2017.
+ */
+public class AtomicCounterTutor {
+
+    //    int counter = 0;
+    AtomicInteger counter = new AtomicInteger();
+
+    class TestThread implements Runnable {
+        String threadName;
+
+        public TestThread(String threadName) {
+            this.threadName = threadName;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10_000; i++) {
+//                counter++;
+                counter.incrementAndGet();
+                Thread.yield();
+            }
+        }
+    }
+
+    @Test
+    public void testThread() {
+        List<Thread> threads = new ArrayList<Thread>();
+        for (int i = 0; i < 100; i++) {
+            threads.add(new Thread(new TestThread("t" + i)));
+        }
+        System.out.println("Starting threads");
+        for (Thread t : threads) {
+            t.start();
+        }
+        try {
+            for (Thread t : threads) {
+                t.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Counter=" + counter);
+
+    }
+}
