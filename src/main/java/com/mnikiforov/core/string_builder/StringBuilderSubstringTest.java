c937b3136a67544ce36e48d488a49b15a6558d37
zigzzzag <maximnikiforov1991@gmail.com>
2017-09-07 10:44:58 +0700

    add string builder substring

---
+++ src/main/java/com/mnikiforov/core/string_builder/StringBuilderSubstringTest.java
@@ -0,0 +1,9 @@
+package com.mnikiforov.core.string_builder;
+
+public class StringBuilderSubstringTest {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Welcome to Hell!");
+        System.out.println(sb.substring(0, 7));
+    }
+}
