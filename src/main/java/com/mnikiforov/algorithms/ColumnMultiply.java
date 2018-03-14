051c2050de3167c934b4d7e7d366fc20ca354465
zigzzzag <maximnikiforov1991@gmail.com>
2018-03-15 00:31:18 +0700

    fix some bugs

--- src/main/java/com/mnikiforov/algorithms/ColumnMultiply.java
+++ src/main/java/com/mnikiforov/algorithms/ColumnMultiply.java
@@ -7,7 +7,7 @@ public class ColumnMultiply implements MultiplyAlgorithm {
         byte[] numeralArr1 = strToNumeralArr(arg1);
         byte[] numeralArr2 = strToNumeralArr(arg2);
 
-        byte[] sumColumnArr = new byte[numeralArr1.length + numeralArr2.length + 1];
+        byte[] sumColumnArr = new byte[numeralArr1.length + numeralArr2.length];
         int sumColumnArrDelta = 0;
         for (int j = numeralArr2.length - 1; j >= 0; j--) {
 
@@ -15,20 +15,21 @@ public class ColumnMultiply implements MultiplyAlgorithm {
             for (int i = numeralArr1.length - 1; i >= 0; i--) {
                 byte m = (byte) (numeralArr2[i] * numeralArr1[j]);
                 if (m < 10) {
-                    columnArrJ[i] += m;
+                    columnArrJ[i + 1] += m;
                 } else {
-                    columnArrJ[i] += m % 10;
-                    columnArrJ[i - 1] += m / 10;
+                    columnArrJ[i + 1] += m % 10;
+                    columnArrJ[i] += m / 10;
                 }
             }
 
             for (int i = columnArrJ.length - 1; i >= 0; i--) {
-                byte s = (byte) (sumColumnArr[i - sumColumnArrDelta] + columnArrJ[i]);
+                int sumColumnArrPos = sumColumnArr.length - 1 -(columnArrJ.length - 1 - i) - sumColumnArrDelta;
+                byte s = (byte) (sumColumnArr[sumColumnArrPos] + columnArrJ[i]);
                 if (s < 10) {
-                    sumColumnArr[i - sumColumnArrDelta] += s;
+                    sumColumnArr[sumColumnArrPos] = s;
                 } else {
-                    sumColumnArr[i - sumColumnArrDelta] += s % 10;
-                    sumColumnArr[i - sumColumnArrDelta - 1]++;
+                    sumColumnArr[sumColumnArrPos] = (byte) (s % 10);
+                    sumColumnArr[sumColumnArrPos - 1]++;
                 }
             }
             sumColumnArrDelta++;
@@ -39,9 +40,19 @@ public class ColumnMultiply implements MultiplyAlgorithm {
 
     private String numeralArrToStr(byte[] numeralArr) {
         StringBuilder sb = new StringBuilder();
-        for (byte b : numeralArr) {
-            sb.append(b);
+
+        boolean firstIsZero = true;
+        for (int i = 0; i < numeralArr.length; i++) {
+            if (numeralArr[i] == 0) {
+                if (!firstIsZero) {
+                    sb.append(numeralArr[i]);
+                }
+            } else {
+                sb.append(numeralArr[i]);
+                firstIsZero = false;
+            }
         }
+
         return sb.toString();
     }
 
---
+++ src/test/java/com/mnikiforov/algorithms/ColumnMultiplyTest.java
@@ -0,0 +1,18 @@
+package com.mnikiforov.algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ColumnMultiplyTest {
+    private final MultiplyAlgorithm multiplyAlgorithm = new ColumnMultiply();
+
+    @Test
+    public void multiplyTest() {
+        assertEquals("4", multiplyAlgorithm.multiply("2", "2"));
+        assertEquals("121", multiplyAlgorithm.multiply("11", "11"));
+        assertEquals("251460", multiplyAlgorithm.multiply("635", "396"));
+        assertEquals("251460", multiplyAlgorithm.multiply("1234567890", "987654321"));
+
+    }
+}
